16a68bede8b2188c8d5951c7da4b80500701506c
Richard Wales <richard_wales@knights.ucf.edu>
2016-10-07 03:44:03 -0400

    Basic project started. Can add to class list

---
+++ scheduler.py
@@ -0,0 +1,40 @@
+import sys
+
+#from PySide.QtCore import 
+from PySide.QtGui import QMainWindow, QApplication, QLabel, QStyleFactory
+from schedui import Ui_Schedule 
+
+class Scheduler(QMainWindow, Ui_Schedule):
+    def __init__(self):
+        super(Scheduler, self).__init__()
+        self.setupUi(self)
+        self.addToScheduleBtn.clicked.connect(self.newCourse)
+        self.addClassBtn.clicked.connect(self.addClassToList)
+        self.removeBtn.clicked.connect(self.removeFromList)
+        self.classInList = set()
+        self.show()
+
+    def removeFromList(self):
+        name = self.classList.currentItem().text() 
+        self.classList.takeItem(self.classList.currentRow())
+        self.classSelector.clear()
+        self.classInList.remove(name)
+        self.classSelector.addItems(list(self.classInList))
+
+    def addClassToList(self):
+        name = self.courseNameText.toPlainText()
+        self.classList.addItem(name)
+        self.classSelector.addItem(name)
+        self.classInList.add(name)
+
+    def newCourse(self):
+        self.scheduleList.insertItem(0, self.classSelector.currentText())
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyle(QStyleFactory.create('Plastique'))
+    s = Scheduler()
+    ret = app.exec_()
+    sys.exit(ret)
+
+        
